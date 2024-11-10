377d17582d41cc2808b5cc0c5d2c2fef570a5875
shaikrasheed99 <shaikrasheed3014@gmail.com>
2024-11-10 23:32:16 +0530

    Add. Name in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>Your Name</h1>
+      <h1>Rasheed Shaik</h1>
       <nav>
         <a href="#about">About</a> | <a href="#projects">Projects</a> | <a href="#contact">Contact</a>
       </nav>
